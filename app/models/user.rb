84430991332b8d99743c29725e3415df782d48de
nishioka-teruaki <nisioka3@msn.com>
2020-10-21 21:21:18 +0900

    ユーザー管理機能の実装

---
+++ app/models/user.rb
@@ -0,0 +1,20 @@
+class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
+  
+  VALID_NAME_REGEX = /\A[ぁ-んァ-ン一-龥]/ # 全角ひらがな漢字
+  VALID_NAME_KANA_REGEX = /\A[ァ-ヶー－]+\z/ # 全角カタカナ
+  VALID_PASSWORD_REGEX =/\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i.freeze # 英数字混合
+
+  validates :nickname, presence: true
+  validates :last_name, presence: true, format: { with: VALID_NAME_REGEX }
+  validates :first_name, presence: true, format: { with: VALID_NAME_REGEX }
+  validates :last_name_kana, presence: true, format: { with: VALID_NAME_KANA_REGEX }
+  validates :first_name_kana, presence: true, format: { with: VALID_NAME_KANA_REGEX }
+  validates :user_birth_date, presence: true
+
+  validates :password, presence: true,format: { with: VALID_PASSWORD_REGEX }
+
+end
