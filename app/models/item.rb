68ce4a1c67602c6991aa317848120279c3c5bb86
nishioka-teruaki <nisioka3@msn.com>
2020-10-26 20:30:47 +0900

    商品出品機能のコミット

---
+++ app/models/item.rb
@@ -0,0 +1,36 @@
+class Item < ApplicationRecord
+  has_one_attached :image
+  belongs_to :user
+
+  with_options presence: true do
+    validates :name, length: { maximum: 40 }
+    validates :info, length: { maximum: 1000 }
+    validates :category_id, numericality: { greater_than: 1 }
+    validates :sales_status_id, numericality: { greater_than: 1 }
+    validates :shipping_fee_status_id, numericality: { greater_than: 1 }
+    validates :item_prefecture_id, numericality: { greater_than: 1 }
+    validates :scheduled_delivery_id, numericality: { greater_than: 1 }
+    validates :price, inclusion: { in: (300..9_999_999) }, numericality: { only_integer: true }
+  end
+
+  validates :image, presence: true, unless: :was_attached?
+  def was_attached?
+    image.attached?
+  end
+
+  extend ActiveHash::Associations::ActiveRecordExtensions do
+    belongs_to_active_hash :category
+    belongs_to_active_hash :sales_status
+    belongs_to_active_hash :shipping_fee_status
+    belongs_to_active_hash :item_prefecture
+    belongs_to_active_hash :scheduled_delivery
+
+    with_options numericality: { other_than: 1 } do
+      validates :category_id
+      validates :sales_status_id
+      validates :shipping_fee_status_id
+      validates :item_prefecture_id
+      validates :scheduled_delivery_id
+    end
+  end
+end
---
+++ spec/models/item_spec.rb
@@ -0,0 +1,74 @@
+require 'rails_helper'
+
+RSpec.describe Item, type: :model do
+  before do
+    @item = FactoryBot.build(:item)
+  end
+
+  describe '商品出品登録' do
+    context '新規登録がうまくいくとき' do
+      it 'image, name、info、category_id、sales_status_id、shipping_fee_status_id、
+          item_prefecture_id、scheduled_delivery_id、price、userが存在すれば登録できる' do
+        expect(@item).to be_valid
+      end
+    end
+
+    context '新規登録がうまくいかないとき' do
+      it 'imageが空では保存できないこと' do
+        @item.image = nil
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Image can't be blank")
+      end
+      it 'nameが空では保存できないこと' do
+        @item.name = nil
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Name can't be blank")
+      end
+      it 'infoが空では保存できないこと' do
+        @item.info = nil
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Info can't be blank")
+      end
+      it 'category_idが1では保存できないこと' do
+        @item.category_id = 1
+        @item.valid?
+        expect(@item.errors.full_messages).to include('Category must be greater than 1')
+      end
+      it 'sales_status_idが1では保存できないこと' do
+        @item.sales_status_id = 1
+        @item.valid?
+        expect(@item.errors.full_messages).to include('Sales status must be greater than 1')
+      end
+      it 'shipping_fee_status_idが1では保存できないこと' do
+        @item.shipping_fee_status_id = 1
+        @item.valid?
+        expect(@item.errors.full_messages).to include('Shipping fee status must be greater than 1')
+      end
+      it 'item_prefecture_idが1では保存できないこと' do
+        @item.item_prefecture_id = 1
+        @item.valid?
+        expect(@item.errors.full_messages).to include('Item prefecture must be greater than 1')
+      end
+      it 'scheduled_delivery_idが1では保存できないこと' do
+        @item.scheduled_delivery_id = 1
+        @item.valid?
+        expect(@item.errors.full_messages).to include('Scheduled delivery must be greater than 1')
+      end
+      it 'priceが空では保存できないこと' do
+        @item.price = nil
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Price can't be blank")
+      end
+      it 'priceが¥300~¥9,999,999の間でないと保存できないこと' do
+        @item.price = 299
+        @item.valid?
+        expect(@item.errors.full_messages).to include('Price is not included in the list')
+      end
+      it 'priceが半角英数でないと保存できないこと' do
+        @item.price = 'あいう'
+        @item.valid?
+        expect(@item.errors.full_messages).to include('Price is not a number')
+      end
+    end
+  end
+end
