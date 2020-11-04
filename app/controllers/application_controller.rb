2405ca2e0d4ce4df876d9fb63f57b1f7c5782371
nishioka-teruaki <nisioka3@msn.com>
2020-11-04 20:42:03 +0900

    環境変数

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,7 @@
 class ApplicationController < ActionController::Base
   before_action :configure_permitted_parameters, if: :devise_controller?
   before_action :authenticate_user!, only: [:new, :edit]
+  before_action :basic_auth
 
   private
 
@@ -10,4 +11,11 @@ class ApplicationController < ActionController::Base
                                              :last_name_kana, :first_name_kana,
                                              :user_birth_date])
   end
+
+  def basic_auth
+    authenticate_or_request_with_http_basic do |username, password|
+      username == ENV["BASIC_AUTH_USER"] && password == ENV["BASIC_AUTH_PASSWORD"]  # 環境変数を読み込む記述に変更
+    end
+  end
+
 end
